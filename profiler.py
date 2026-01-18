78a98ecd967dfa32edb01f9387d2f300ca545a46
a1b3r <87335034+a1b3r@users.noreply.github.com>
2026-01-18 09:40:51 -0600

    Update profiler.py

--- profiler.py
+++ profiler.py
@@ -1,10 +1,7 @@
-import json
 import timeit
 import cProfile
 import pstats
 from memory_profiler import memory_usage
-from data_loader import load_market_data_csv
-from strategies import NaiveMovingAverageStrategy, WindowedMovingAverageStrategy
 
 def run_strategy(strategy, data, n):
     """Runs update() for first n ticks."""
@@ -14,7 +11,6 @@ def run_strategy(strategy, data, n):
 def time_one(strategy_factory, data, n, repeats=3):
     """
     Measures execution time using timeit.
-    strategy_factory must return a NEW strategy each time.
     """
     stmt = lambda: run_strategy(strategy_factory(), data, n)
     times = timeit.repeat(stmt, repeat=repeats, number=1)
@@ -33,7 +29,7 @@ def peak_memory_one(strategy_factory, data, n):
 def cprofile_one(strategy_factory, data, n, prof_out_path, top_n=15):
     """
     Runs cProfile and saves results to a .prof file.
-    Also prints the top functions by total time.
+
     """
     profiler = cProfile.Profile()
     profiler.enable()
@@ -43,4 +39,4 @@ def cprofile_one(strategy_factory, data, n, prof_out_path, top_n=15):
     profiler.dump_stats(prof_out_path)
 
     stats = pstats.Stats(profiler).sort_stats("tottime")
-    stats.print_stats(top_n)
+    return stats.print_stats(top_n)
